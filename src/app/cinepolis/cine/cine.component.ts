294a76a31e264fb8651fc84ba7e883be5397ef07
Antonio Ponce <19002468@alumnos.utleon.edu.mx>
2024-09-24 18:12:03 -0600

    24/09/2024 - Cinepolis

--- src/app/cinepolis/cine/cine.component.ts
+++ src/app/cinepolis/cine/cine.component.ts
@@ -1,34 +1,104 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators } from "@angular/forms";
-
+interface ResultadoCompra {
+  total: number;  
+  nombre: string;
+  numeroBoletos: number;
+}
 @Component({
   selector: 'app-cine',
   templateUrl: './cine.component.html',
   styleUrl: './cine.component.css'
 })
 export class CineComponent {
-  nBoletos!:number;
-  nombre!:string;
-  isCineco!:boolean;
-  nCompradores!:number;
-  precioBoleto:number=12;
-  formulario!:FormGroup;
-  resultado!:number;
-  constructor(){}
+  precioBoleto: number = 12;
+  formulario!: FormGroup;
+  resultado: ResultadoCompra | null = null;
+  resultado2!:boolean;
+  constructor() {}
+
   ngOnInit(): void {
     this.formulario = new FormGroup({
-    nombre: new FormControl('',Validators.required),
-    nBoletos: new FormControl('',Validators.required),
-    nCompradores: new FormControl('',Validators.required),
-    isCineco : new FormControl('',Validators.required),
-  });
+      nombre: new FormControl('', Validators.required),
+      nBoletos: new FormControl('', Validators.required),
+      nCompradores: new FormControl('', Validators.required),
+      isCineco: new FormControl('', Validators.required),
+    });
   }
 
-  vende():void{
-    let nombre=this.formulario.get('nombre')?.value;
-    let nBoletos=this.formulario.get('nBoletos')?.value;
-    let isCineco=this.formulario.get('isCineco')?.value;
-    let nCompradores=this.formulario.get('nCompradores')?.value;
-    this.resultado=nBoletos*this.precioBoleto;
+  vende(): void {
+    let nombre = this.formulario.get('nombre')?.value;
+    let nBoletos = this.formulario.get('nBoletos')?.value;
+    let isCineco = this.formulario.get('isCineco')?.value;
+    let nCompradores = this.formulario.get('nCompradores')?.value;
+
+    let totalFinal=0;
+    let totalFinalCineco=0;
+    let total = nBoletos * this.precioBoleto;
+    let subtotal = nBoletos * this.precioBoleto;
+    let boletosComprados = nBoletos;
+    let boletosMaximo = nCompradores * 7;
+    console.log("Boletos comprados",boletosComprados);
+    console.log("Boletos Validacion",boletosMaximo);
+    console.log("isCineco",isCineco);
+    if (boletosMaximo>=boletosComprados) {
+      
+      if (nBoletos > 5) {
+        total *= 0.15; 
+        totalFinal= subtotal-total;
+        if (isCineco=='si') {
+          totalFinalCineco=totalFinal-(totalFinal * 0.10); 
+
+          this.resultado = {
+            total: totalFinalCineco,
+            nombre: nombre,
+            numeroBoletos: nBoletos};
+            console.log(boletosComprados);
+        }else{
+        this.resultado = {
+          total: totalFinal,
+          nombre: nombre,
+          numeroBoletos: nBoletos};
+          console.log(boletosComprados);
+        }
+      } else if (nBoletos >= 3 && nBoletos <= 5) {
+        totalFinal=total-(total * 0.10); 
+        this.resultado = {
+          total: totalFinal,
+          nombre: nombre,
+          numeroBoletos: nBoletos};
+          console.log(boletosComprados);
+        }else if (nBoletos<3) {
+          totalFinal=total;
+          if (isCineco=='si') {
+            totalFinalCineco=totalFinal-(totalFinal * 0.10); 
+  
+            this.resultado = {
+              total: totalFinalCineco,
+              nombre: nombre,
+              numeroBoletos: nBoletos};
+              console.log(boletosComprados);
+          }else{
+          this.resultado = {
+            total: totalFinal,
+            nombre: nombre,
+            numeroBoletos: nBoletos};
+            console.log(boletosComprados);
+          }
+        }
+      
+  
+      
+      
+      
+    }else{
+      this.resultado2 = true;
+    }
+    
+
+  
+    
+    
   }
+  
 }
